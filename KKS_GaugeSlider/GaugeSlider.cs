ad0dbbcfe8615e308887c63c4734cae76117f47e
UnkoMan <91043119+hyperunkoman@users.noreply.github.com>
2021-09-20 17:19:45 +0900

    プロジェクト ファイルを追加します。

---
+++ KKS_GaugeSlider/GaugeSlider.cs
@@ -0,0 +1,114 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using RuntimeUnityEditor.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KKS_GaugeSlider
+{
+	[BepInPlugin("GaugeSlider", "Gauge Slider", "0.1")]
+	[BepInDependency(RuntimeUnityEditorCore.GUID, RuntimeUnityEditorCore.Version)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID, KKAPI.KoikatuAPI.VersionConst)]
+    public class GaugeSlider : BaseUnityPlugin
+    {
+		internal static new ManualLogSource Logger;
+
+		private HFlag m_hFlag;
+
+		public GaugeSlider()
+        {
+			Logger = base.Logger;
+		}
+
+		public void Start()
+        {
+            KKAPI.MainGame.GameAPI.StartH += GameAPI_StartH;
+			KKAPI.MainGame.GameAPI.EndH += GameAPI_EndH;
+		}
+
+        private void GameAPI_EndH(object sender, System.EventArgs e)
+        {
+			//Logger.LogDebug(string.Format("EndH"));
+		}
+
+        private void GameAPI_StartH(object sender, System.EventArgs e)
+        {
+			m_hFlag = Object.FindObjectOfType<HFlag>();
+			//Logger.LogDebug(string.Format("StartH, flag=%b", m_hFlag != null));
+			SetSlider_H();
+		}
+
+		private void SetSlider_H()
+		{
+			const float fgauge_size = 130.0f;
+			const float fgauge_width = 11.0f;
+			const float mgauge_size = 150.0f;
+			const float mgauge_width = 10.0f;
+			const float center_x_off = 22.0f;
+			const float center_y_off = -38.0f;
+			const float gauge_val_off = -0.04f;
+			const float gauge_val_factor = 2.18f;
+			Vector2 centerOffset = new Vector2(center_x_off, center_y_off);
+
+			if (m_hFlag == null)
+			{
+				return;
+			}
+			GameObject gaugeFrame = GameObject.Find("Canvas/Gauge/Frame");
+			if (gaugeFrame != null && !gaugeFrame.transform.Find("Radial Slider"))
+			{
+				GameObject femaleSlider = UI.CreateRadialSlider(gaugeFrame);
+				RectTransform femaleTf = femaleSlider.GetComponent<RectTransform>();
+				RadialSlider femaleUI = femaleSlider.GetComponent<RadialSlider>();
+				femaleUI.Thickness = fgauge_width;
+				femaleUI.Radius = fgauge_size;
+				femaleUI.CenterOffset = centerOffset;
+				femaleUI.onClick = delegate (float x)
+				{
+					//Logger.LogMessage(string.Format("Female gauge: {0}", x));
+					x = (1.0f + x + gauge_val_off) * gauge_val_factor;
+					m_hFlag.gaugeFemale = Mathf.Clamp(x * 100f, 0f, 99.9f);
+				};
+				femaleTf.anchoredPosition = Vector2.zero;
+				GameObject maleSlider = UI.CreateRadialSlider(gaugeFrame);
+				RectTransform maleTf = maleSlider.GetComponent<RectTransform>();
+				RadialSlider maleUI = maleSlider.GetComponent<RadialSlider>();
+				maleUI.Thickness = mgauge_width;
+				maleUI.Radius = mgauge_size;
+				maleUI.CenterOffset = centerOffset;
+				maleUI.onClick = delegate (float x)
+				{
+					//Logger.LogMessage(string.Format("Male gauge: {0}", x));
+					x = (1.0f + x + gauge_val_off) * gauge_val_factor;
+					m_hFlag.gaugeMale = Mathf.Clamp(x * 100f, 0f, 99.9f);
+				};
+				maleTf.anchoredPosition = Vector2.zero;
+				Image[] componentsInChildren = GameObject.Find("Canvas/Gauge/Female").GetComponentsInChildren<Image>();
+				for (int i = 0; i < componentsInChildren.Length; i++)
+				{
+                    componentsInChildren[i].raycastTarget = (false);
+				}
+				componentsInChildren = GameObject.Find("Canvas/Gauge/Male").GetComponentsInChildren<Image>();
+				for (int i = 0; i < componentsInChildren.Length; i++)
+				{
+                    componentsInChildren[i].raycastTarget = (false);
+				}
+				gaugeFrame.GetComponent<Image>().raycastTarget = (false);
+				//GameObject gameObject4 = GameObject.Find("Canvas/face");
+				//canvasH = gameObject4.transform.parent.GetComponent<Canvas>();
+				//RectTransform component5 = gameObject4.GetComponent<RectTransform>();
+				//UI.CreateButton(gameObject4, new Rect(0f, 0f, component5.sizeDelta.y, component5.sizeDelta.y), string.Empty, delegate
+				//{
+				//	GuiToggle();
+				//}, Color.black, Color.clear, component5.sizeDelta.y * 0.9f);
+
+				var faceRawImage = GameObject.Find("Canvas/face/RawImage");
+                if (faceRawImage != null)
+                {
+					faceRawImage.GetComponent<RawImage>().raycastTarget = false;
+				}
+			}
+		}
+
+	}
+}
---
+++ KKS_GaugeSlider/UI.cs
@@ -0,0 +1,87 @@
+﻿//#define DBG_OUT
+using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+namespace KKS_GaugeSlider
+{
+	public class RadialSlider : MonoBehaviour, IPointerClickHandler, IEventSystemHandler, ICanvasRaycastFilter
+	{
+		public float Thickness { get; set; }
+
+		public float Radius { get; set; }
+
+		public Vector2 CenterOffset { get; set; } = Vector2.zero;
+
+		public Action<float> onClick { get; set; }
+
+#if DBG_OUT
+		private string m_lastStr;
+#endif
+		public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+		{
+#if DBG_OUT
+			var sb = new StringBuilder();
+			sb.AppendFormat("isvalid[{0:X8}]:({1:F0},{2:F0})", this.GetInstanceID(), sp.x, sp.y);
+#endif
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, sp, eventCamera, out sp);
+			float num = Vector2.Distance(sp, CenterOffset);
+			bool isValid = (Radius < num && num < Radius + Thickness);
+#if DBG_OUT
+			sb.AppendFormat("->({0:F0},{1:F0})-> dist:{2:F0} -> {3}", sp.x, sp.y, num, isValid);
+			if (m_lastStr == null || m_lastStr.CompareTo(sb.ToString()) != 0)
+			{
+				m_lastStr = sb.ToString();
+				Console.WriteLine(m_lastStr);
+			}
+#endif
+			return isValid;
+		}
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			GraphicRaycaster componentInParent = GetComponentInParent<GraphicRaycaster>();
+			if (componentInParent != null)
+			{
+				RectTransformUtility.ScreenPointToLocalPointInRectangle(transform as RectTransform, Input.mousePosition, componentInParent.eventCamera, out Vector2 localPoint);
+				localPoint -= CenterOffset;
+				float rate = Mathf.Atan2(0f - localPoint.x, localPoint.y) / Mathf.PI;
+				if (onClick != null)
+				{
+#if DBG_OUT
+					Console.WriteLine(string.Format("Click:{0:F4}", rate));
+#endif
+					onClick(rate);
+				}
+			}
+		}
+	}
+
+	public class UI
+	{
+		public static GameObject CreateRadialSlider(GameObject parent)
+		{
+			GameObject gameObject = new GameObject("Radial Slider");
+			Image obj = gameObject.AddComponent<Image>();
+			obj.sprite = null;
+			obj.type = Image.Type.Filled;
+			obj.color = (Color.clear);
+			obj.fillMethod = Image.FillMethod.Radial360;
+			obj.fillOrigin = (0);
+			obj.fillClockwise = (false);
+			RectTransform component = parent.gameObject.GetComponent<RectTransform>();
+			RectTransform component2 = gameObject.GetComponent<RectTransform>();
+			component2.pivot = Vector2.up;
+			component2.anchorMin = Vector2.up;
+			component2.anchorMax = Vector2.up;
+			component2.sizeDelta = new Vector2(component.sizeDelta.x, component.sizeDelta.y);
+			component2.anchoredPosition = Vector2.zero;
+			gameObject.AddComponent<RadialSlider>();
+			gameObject.transform.SetParent(parent.transform, worldPositionStays: false);
+			return gameObject;
+		}
+	}
+}
